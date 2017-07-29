88cd9eceda44bb3e6719d8272946a61b9a0bdbf2
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-29 19:19:03 +0200

    My variables
    this program give an overview about variables

---
+++ My_variables/My_variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int @my_value=6;
+            Console.Write("x=nnnnnnnn               {0}",@my_value);
+            Console.ReadKey();
+
+        }
+    }
+}
