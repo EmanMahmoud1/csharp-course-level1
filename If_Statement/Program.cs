6a9098f0816cc69666e9490c4a98fbca370ce9af
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-29 21:40:26 +0200

    simple if
    simple if statement

---
+++ If_Statement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            // simple if statment
+            String v1 = "huda";
+            if (v1 == "huda")
+                Console.WriteLine("your name is Huda");
+            else
+                Console.WriteLine("your name is Nada");
+            Console.ReadKey();
+             * 
+             */
+
+            // simple if statment
+            String v1 = "huda";
+            if (v1 == "hua")
+            {
+                Console.WriteLine("your name is Huda");
+                Console.WriteLine("ya huda, please work harder");
+
+
+            }
+            else
+            {
+                Console.WriteLine("your name is Nada");
+                Console.WriteLine("you are a clever girl");
+            }
+            Console.ReadKey();
+             
+        }
+    }
+}
