0b38f6a1950ab985d8d47249d699c28faf08afb7
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-29 21:09:38 +0200

    Nested If statement
    This code illustrate how to use more than one if statment(if inside if).

---
+++ Nested-if-statment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_if_statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 11, v2 = 0;
+            if (v1 >= 2)
+            {
+                Console.WriteLine("your Variable greater than or equals 2");
+                // nested if inside another if.
+                if (v2 < 7)
+                {
+                    Console.WriteLine("v1>=2      v2<7");
+                }
+                    else
+                    {
+                    Console.WriteLine(" v1>=2      v2>=7");
+                    }
+            }
+            else
+            {
+                Console.WriteLine("your variable less than2");
+            }
+            Console.ReadKey();
+        }
+    }
+}
