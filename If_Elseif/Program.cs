61f3bab76c9f126d47a928fb15bae43a170a6f6a
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-29 23:13:58 +0200

    If-Elseif
    how to use if and elseif

---
+++ If_Elseif/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HOW to use if and else if statment
+            int v1=10,v2=14;
+            if (v1 <= 2 && v2 >= 2)
+            {
+                Console.WriteLine("the first condition is true");
+            }
+            else if (v1<=2 || v2>=13)
+            {
+                Console.WriteLine("the second condition is true");
+            }
+            else if (v2 == 12)
+            {
+                Console.WriteLine("the third condition is true");
+
+            }
+            else
+            {
+                Console.WriteLine("try again");
+            }
+
+            Console.ReadKey();
+    
+        }
+    }
+}
