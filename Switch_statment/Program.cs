427ea91b6bc67c29e838b0861ffb3c639ab26d3a
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-29 23:37:33 +0200

    Switch case statment
    how to use switch case

---
+++ Switch_statment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mychildren="nada";
+            // switch syntax
+            switch (mychildren)
+            {
+                    //every case has it's break
+                case "taha":
+                    Console.WriteLine("your baby is taha");
+                    break;
+                case "huda":
+                    Console.WriteLine("huda");
+                    break;
+                case "nada":
+                    Console.WriteLine("nada");
+                    break;
+                default:
+                    Console.WriteLine("no one enter");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
