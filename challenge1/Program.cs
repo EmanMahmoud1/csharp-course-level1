7db667b9a6a5d804f9dede78703284b64d121ce8
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 11:14:28 +0200

    Do_While

---
+++ challenge1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary_now = 1020, salary_final = 2000,salary=salary_now;
+            int i = 0;
+            do
+            {
+                salary += 20;
+                i++;
+                //Console.WriteLine("the number of monthes{0}", i);
+
+            } 
+            while (salary<salary_final);
+            Console.WriteLine("the number of monthes{0}", i);
+            Console.WriteLine("the final salary{0}", salary);
+
+            Console.ReadKey();
+        }
+    }
+}
