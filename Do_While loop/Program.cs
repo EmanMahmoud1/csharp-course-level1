6ab1ddb616ad1d719478a793adbe50fee72a6bb7
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 09:59:56 +0200

    Do_While loop
    How to use do while for repetition

---
+++ Do_While loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salary = 200f, total= 0f;
+            int i=0;
+            do {
+                total=salary+total;// add salary with toatal.
+                 i++;//increment the value of i each turn
+                Console.WriteLine("the value of i =  {0}",i);
+            
+            }while(i<10);
+            Console.WriteLine("your salary through 10 month {0}",total);
+            Console.ReadKey();
+        }
+    }
+}
