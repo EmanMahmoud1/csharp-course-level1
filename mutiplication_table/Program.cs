de56a9c86acea6767ec32f094c58b23773c9c4a9
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 22:10:51 +0200

    multiplication table for odd number
    multiplication table for odd number 1 3  5 7 .....................

--- mutiplication_table/Program.cs
+++ mutiplication_table/Program.cs
@@ -10,11 +10,12 @@ namespace mutiplication_table
     {
         static void Main(string[] args)
         {
+            // multiplication table for odd number
             int mul = 0;
-            for (int i = 1; i < 13; i++)
+            for (int i = 1; i < 13; i+=2)
             {
                 Console.WriteLine("\n");
-                for (int j = 1; j <= 12; j++)
+                for (int j = 1; j <= 12; j+=2)
                 {
                     mul = i * j;
                     Console.WriteLine("     {0}*{1}={2}     ", i, j, mul);
