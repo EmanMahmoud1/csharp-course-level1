36f1a0afd8866bb16c32daeedde5b65fbbd94b15
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 21:12:20 +0200

    Nested for loop
    This code to print matrix  1 2 3
    4 5 6
    7 8 9

---
+++ nested_for_loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 1,coulmnnumber=3,temp1=0;
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = temp; j < coulmnnumber + temp; j++)
+                {
+                    Console.Write("    {0}    ", j);
+                    temp1 = j+1;
+                }
+                temp = temp1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
