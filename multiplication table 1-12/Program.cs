21fca9e735d6b060142bae35e8063cd0cd4d4110
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 22:16:07 +0200

    mulplication table

---
+++ multiplication table 1-12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mutiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // multiplication table for odd number
+            int mul = 0;
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 1; j <= 12; j++)
+                {
+                    mul = i * j;
+                    Console.WriteLine("     {0}*{1}={2}     ", i, j, mul);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
