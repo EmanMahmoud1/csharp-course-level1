df4d296ca75b163f652eaa6a8dd77aa78722965f
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-30 04:21:49 +0200

    ternary_operator 2
    this code check string condition and use variable.Equals()
    function.

---
+++ ternary_condition2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternary_condition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "nada", myname;
+            // name.Equals("nada")   and    name=="nada" work the same 
+           // myname=(name.Equals("nada"))?"your name is Nada":"your name is Huda";
+            myname = (name=="nada") ? "your name is Nada" : "your name is Huda";
+            Console.WriteLine("************{0}***********",myname);
+            Console.ReadKey();
+        }
+    }
+}
