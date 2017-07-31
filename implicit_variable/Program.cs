727f7d33cf044f19067d9c5c1d14b8a582c07832
EmanMahmoud1 <emanexxee@gmail.com>
2017-07-31 10:53:18 +0200

    implicit_variable

---
+++ implicit_variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implicit_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long v1 = 1000;
+            float v2 = v1;
+            float f = 9f;
+            // long l = f;              //Error	1	Cannot implicitly convert type 'float' to 'long'. 
+            //An explicit conversion exists (are you missing a cast?)	
+            Console.WriteLine("{0}",v2);
+            char c='f';
+            int d = c;
+            Console.WriteLine("{0}",d);
+            Console.ReadKey();
+        }
+    }
+}
