2c38881e0288ee447165c3af792ec5cbb3a79014
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-05 01:05:13 +0200

    two dimensional array

---
+++ multidimensional_array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multidimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers1 ={                                                                           //definition1
+
+                                {1,2,3,4},
+                                {3,2,4,5},
+                                {6,12,55,6}
+                            };
+            string[,] names = new string[,] {                                               //definition 2
+            
+            {"taha","huda","nada"},{"eman","mahmoud","abdo"}
+            
+            };
+            int i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4;j++ )
+                {
+                    Console.Write(numbers1[i,j] + "\t");
+
+                };
+                Console.Write("\n");
+
+            };
+           
+            Console.ReadKey();
+            
+
+        }
+    }
+}
