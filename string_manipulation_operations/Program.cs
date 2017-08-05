a3d34542ad0470a002dec31f672f7b06ffb16fce
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-05 16:32:51 +0200

    string_manpulation
    how to convert string into matrix of chars

---
+++ string_manipulation_operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_manipulation_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "i am smart";
+            char[] mychararray = mystring.ToCharArray();
+            Console.WriteLine("your string length is {0}", mystring.Length);
+            foreach (char mychar in mychararray)
+            {
+                Console.WriteLine(mychar);
+            }
+            Console.ReadKey();
+        }
+    }
+}
