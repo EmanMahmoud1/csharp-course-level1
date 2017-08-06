6c5ba529775f42470e9512c2959f5e0a2c00b32b
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-06 11:14:36 +0200

    split function

---
+++ cut_sentence_into_words_using_split/cut_sentence_into_words_using_split/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cut_sentence_into_words_using_split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring;
+            char[] seperator = {' '};
+            Console.WriteLine("enter your sentence");
+            mystring = Console.ReadLine();
+            string[] mywords;
+            mywords = mystring.Split(seperator);
+            foreach(string word in mywords)
+            {
+            
+            Console.Write(word+"    ");
+            
+            }
+            Console.ReadKey();
+        }
+    }
+}
