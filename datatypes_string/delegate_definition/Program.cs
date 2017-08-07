c50070119f24852c1511047e4273ccfb2f965422
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-07 11:35:35 +0200

    delegate

---
+++ datatypes_string/delegate_definition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_definition
+{
+    delegate int mynewnumber(int num);
+    class Program
+    {
+        static int mynum = 3;
+        static int addfunction(int number)
+        {
+            mynum += number;
+            return mynum;
+        }
+        static int mulfunction(int number)
+        {
+            mynum *= number;
+            return mynum;
+        }
+        static void Main(string[] args)
+        {
+            mynewnumber n1 = new mynewnumber(addfunction);
+            mynewnumber n2 = new mynewnumber(mulfunction);
+            n1(5);
+            Console.WriteLine(mynum);
+            n2(5);
+            Console.WriteLine(mynum);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
