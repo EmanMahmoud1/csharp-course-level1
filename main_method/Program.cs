7ddc5500c5c4d24d09c553900a236e46d846940b
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-07 05:05:26 +0200

    focus on main method

---
+++ main_method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_method
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("the argument length = {0}",args.Length);
+            foreach(string str in args )
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
