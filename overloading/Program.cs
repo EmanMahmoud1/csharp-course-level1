bceed41ad77321ea27066738f1bba737a4ed5152
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-07 10:41:23 +0200

    overloading

---
+++ overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading
+{
+    class Program
+    {
+        //2 function with the same name and different function type and parameters.
+        public static int printnumber(int mynumber)
+        {
+            return mynumber;
+        }
+        public static double printnumber(double mynumber)
+        {
+            return mynumber;
+        }
+        static void Main(string[] args)
+        {
+          int num=6;
+            Console.WriteLine(printnumber(num));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ struct_function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_function
+{
+    class Program
+    {
+        struct fullname
+        {
+            public string firstname, lastname;
+        }
+        static void Main(string[] args)
+        {
+            fullname name;
+            name.firstname = "eman";
+            name.lastname = "abd elhaleim";
+            Console.WriteLine("{0}  {1}",name.firstname,name.lastname);
+            Console.ReadKey();
+        }
+    }
+}
