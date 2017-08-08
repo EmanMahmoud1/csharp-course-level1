c3c7f9a3190378e10cbc0cc014d2d803d5a37dbc
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-08 14:07:13 +0200

    assertion method of debuging

---
+++ firstprogram/assertion_methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+
+namespace assertion_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("enter your value");
+            int i = Convert.ToInt32(ReadLine());
+            if ( i>= 100)
+            {
+                WriteLine($"{i}");
+            }
+            else
+            {
+               // Debug.WriteLine("we are here");
+                Debug.Assert(i>=100,"account failure","your accout must be greater than 100");
+                //Trace.Assert(i >= 100, "short", "long");
+
+            }
+            WriteLine("Done");
+            ReadKey();
+        }
+    }
+}
