9df57ea3e9710d441bf5f7ece39810e026ec2012
EmanMahmoud1 <emanexxee@gmail.com>
2017-08-10 17:46:42 +0200

    params used in function

---
+++ paramsReferances/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace paramsReferances
+{
+    class Program
+    {
+        static double CalculateAverage(params double[] values)
+        {
+            WriteLine("you send to me {0} double values",values.Length);
+            double sum = 0,avg;
+            if (values.Length == 0)
+            {
+                return sum;
+            }
+
+            foreach (double num in values)
+                {
+                sum += num;
+
+            }
+            avg = sum / values.Length;
+
+            
+            return avg;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            double average;
+            average = CalculateAverage(2.4,5,8.65,8);
+            WriteLine($"the average equal {average}");
+
+            double [] average1= {2.3,4,5,6,6.1 };
+            double average11 = CalculateAverage(average1);
+            WriteLine($"the average equal {average11}");
+            double average2;
+            average2 = CalculateAverage();
+            WriteLine($"the average equal {average2}");
+            ReadKey();
+        }
+    }
+}
